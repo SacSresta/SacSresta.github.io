4f229e4abd9f8dfce6cb4f6c1b35c525be13d709
SacSresta <141592782+SacSresta@users.noreply.github.com>
2025-02-10 18:50:16 +1100

    Update flask_app.py

--- netlify/functions/flask_app.py
+++ netlify/functions/flask_app.py
@@ -1,3 +1,10 @@
+# netlify/functions/flask_app.py
+import os
+import sys
+
+# Insert the root folder into sys.path so that app.py can be imported
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 from app import app
 from serverless_wsgi import handle_request
 
